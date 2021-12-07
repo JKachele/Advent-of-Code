cbdebd080674132be7b6f7464e0e467351150292
Justin Kachele <justin@kachele.com>
2021-12-07 15:03:24 -0600

    initial commit

---
+++ src/com/jkachele/aoc/_2021/day8/Day8b.java
@@ -0,0 +1,20 @@
+package com.jkachele.aoc._2021.day8;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day8b {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("E:\\JavaProjects\\Advent-of-Code\\src\\com\\jkachele\\aoc\\_2021\\day[day]\\input.txt");
+        Scanner fileIn = new Scanner(file);
+        ArrayList<String> lines = new ArrayList<>();
+
+        while (fileIn.hasNext()) {
+            lines.add(fileIn.nextLine());
+        }
+        fileIn.close();
+        System.out.println(lines);
+    }
+}
