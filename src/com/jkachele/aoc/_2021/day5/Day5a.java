cc41c34c1ac909e9799504042baddb0e083bcd1b
Justin Kachele <justin@kachele.com>
2021-12-07 11:56:02 -0600

    Added the lines to the diagram

--- src/com/jkachele/aoc/_2021/day5/Day5a.java
+++ src/com/jkachele/aoc/_2021/day5/Day5a.java
@@ -36,7 +36,29 @@ public class Day5a {
             System.out.printf("[%s, %s], ", Arrays.toString(line[0]), Arrays.toString(line[1]));
         }
 
+        //2d array of points to track covered points
+        int[][] diagram = new int[1000][1000];
 
-
+        //loops through all the lines and adds them to the diagram
+        for(int[][] line : lines) {
+            //if X coordinate is the same, then it's a vertical segment
+            if(line[0][0] == line[1][0]) {
+                int x = line[0][0];
+                int minY = Math.min(line[0][1], line[1][1]);
+                int maxY = Math.max(line[0][1], line[1][1]);
+                //loops through the points on the line from the start to the end point and adds one to the diagram
+                for(int y=minY; y<=maxY; y++)
+                    diagram[x][y]++;
+            }
+            //if Y coordinate is the same, then it's a horizontal segment
+            if(line[0][1] == line[1][1]) {
+                int y = line[0][1];
+                int minX = Math.min(line[0][0], line[1][0]);
+                int maxX = Math.max(line[0][0], line[1][0]);
+                //loops through the points on the line from the start to the end point and adds one to the diagram
+                for(int x=minX; x<=maxX; x++)
+                    diagram[x][y]++;
+            }
+        }
     }
 }
