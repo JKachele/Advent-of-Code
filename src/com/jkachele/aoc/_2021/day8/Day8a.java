3909a13be8f21d4761a9ab5dd92d9f4387feeb0f
Justin Kachele <justin@kachele.com>
2021-12-07 23:56:18 -0600

    initial commit

--- src/com/jkachele/aoc/_2021/day8/Day8a.java
+++ src/com/jkachele/aoc/_2021/day8/Day8a.java
@@ -7,14 +7,32 @@ import java.util.Scanner;
 
 public class Day8a {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("E:\\JavaProjects\\Advent-of-Code\\src\\com\\jkachele\\aoc\\_2021\\day[day]\\input.txt");
+        File file = new File("E:\\JavaProjects\\Advent-of-Code\\src\\com\\jkachele\\aoc\\_2021\\day8\\input.txt");
         Scanner fileIn = new Scanner(file);
         ArrayList<String> lines = new ArrayList<>();
 
-        while (fileIn.hasNext()) {
+        while(fileIn.hasNext()) {
             lines.add(fileIn.nextLine());
         }
         fileIn.close();
         System.out.println(lines);
+
+        String[][] digits = new String[lines.size()][4];
+
+        for(int i=0; i<lines.size(); i++) {
+            String[] values = lines.get(i).split(" \\| ");
+            String[] outputValues = values[1].split(" ");
+            digits[i] = outputValues;
+        }
+
+        int numUniqueDigits = 0;
+        for(String[] digitSet : digits) {
+            for(String digit: digitSet) {
+                if(digit.length() == 2 || digit.length() == 3 || digit.length() == 4 || digit.length() == 7)
+                    numUniqueDigits++;
+            }
+        }
+
+        System.out.println(numUniqueDigits);
     }
 }
