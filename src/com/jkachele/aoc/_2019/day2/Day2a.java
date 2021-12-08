8c24d046ce10f08d63702f885fb88738b3a55257
Justin Kachele <justin@kachele.com>
2021-12-07 23:43:22 -0600

    initial commit

--- src/com/jkachele/aoc/_2019/day2/Day2a.java
+++ src/com/jkachele/aoc/_2019/day2/Day2a.java
@@ -1,71 +1,29 @@
 package com.jkachele.aoc._2019.day2;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
 
 public class Day2a {
-
     public static void main(String[] args) throws Exception{
-        Scanner fileIn = new Scanner(new File("Text Docs\\2019Day2.txt"));
-        ArrayList<Integer> code = new ArrayList<>();
+        File file = new File("E:\\JavaProjects\\Advent-of-Code\\src\\com\\jkachele\\aoc\\_2019\\day2\\input.txt");
+        Scanner fileIn = new Scanner(file);
 
-        String input = fileIn.nextLine();
-        boolean quit = false;
-        boolean error = false;
-        String intcode = input.replace(',', ' ');
-        Scanner codeRead = new Scanner(intcode);
+        String line = fileIn.nextLine();
+        fileIn.close();
 
-        while(codeRead.hasNextInt()) {
-            code.add(codeRead.nextInt());
-        }
+        String[] intCodeStrings = line.split(",");
+        int[] intCode = new int[intCodeStrings.length];
 
-        codeRead.close();
+        for(int i=0; i<intCode.length; i++)
+            intCode[i] = Integer.parseInt(intCodeStrings[i]);
 
-        code.set(1, 12);
-        code.set(2, 2);
+        System.out.println(Arrays.toString(intCode));
 
-        //System.out.println(code.size());
-        for(int i = 0; i < code.size(); i += 4) {
-            int opcode = code.get(i);
-            //System.out.print(i+", "+opcode+": ");
-            switch(opcode) {
-                case 1:
-                    code.set(i+3, add(code, code.get(i+1), code.get(i+2)));
-                    System.out.println(i+" sum of "+code.get(i+3)+" at "+(i+3));
-                    break;
-                case 2:
-                    code.set(i+3, mult(code, code.get(i+1), code.get(i+2)));
-                    System.out.println(i+" prod of "+code.get(i+3)+" at "+(i+3));
-                    break;
-                case 99:
-                    quit = true;
-                    break;
-                default:
-                    System.out.println("Error at Index "+i);
-                    quit = true;
-                    error = true;
-                    break;
-            }
-            if(quit)
-                break;
-        }
-
-        if(!error) {
-            System.out.println(code.get(0));
-            System.out.println(code);
-        }
 
     }
 
-    public static int add (ArrayList<Integer> array, int num1, int num2) {
-        int sum = num1 + num2;
-        return sum;
-    }
+    public static void add(int[] intCode, int index) {
 
-    public static int mult (ArrayList<Integer> array, int num1, int num2) {
-        int prod = num1 * num2;
-        return prod;
     }
-
 }
