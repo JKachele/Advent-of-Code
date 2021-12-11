08ad33dedb837ef5c71fa171c2228d99cff9a71e
Justin Kachele <justin@kachele.com>
2021-12-11 02:45:53 -0600

    initial commit

--- src/com/jkachele/aoc/_2021/day10/Day10b.java
+++ src/com/jkachele/aoc/_2021/day10/Day10b.java
@@ -3,10 +3,11 @@ package com.jkachele.aoc._2021.day10;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Day10a {
+public class Day10b {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("E:\\JavaProjects\\Advent-of-Code\\src\\com\\jkachele\\aoc\\_2021\\day10\\input.txt");
         Scanner fileIn = new Scanner(file);
@@ -19,13 +20,13 @@ public class Day10a {
         System.out.println(lines);
 
         String openChars = "([{<";
-        int totalErrorScore = 0;
+        ArrayList<Long> completionScores = new ArrayList<>();
 
         for(String line : lines) {
             Stack<Character> stack = new Stack<>();
+            boolean corrupted = false;
             for(int i=0; i<line.length(); i++) {
                 char c = line.charAt(i);
-                int errorScore = 0;
                 if(openChars.indexOf(c) != -1) {
                     stack.add(c);
                 } else {
@@ -35,39 +36,76 @@ public class Day10a {
                                 if (stack.peek() == '(')
                                     stack.pop();
                                 else
-                                    errorScore = 3;
+                                    corrupted = true;
                                 break;
                             case ']':
                                 if (stack.peek() == '[')
                                     stack.pop();
                                 else
-                                    errorScore = 57;
+                                    corrupted = true;
                                 break;
                             case '}':
                                 if (stack.peek() == '{')
                                     stack.pop();
                                 else
-                                    errorScore = 1197;
+                                    corrupted = true;
                                 break;
                             case '>':
                                 if (stack.peek() == '<')
                                     stack.pop();
                                 else
-                                    errorScore = 25137;
+                                    corrupted = true;
                                 break;
                             default:
                                 System.out.println("Error!");
                         }
-                        if(errorScore != 0) {
-                            totalErrorScore += errorScore;
-                            break;
-                        }
                     }
                 }
+                if(corrupted)
+                    break;
             }
+
+            if(!corrupted && !stack.isEmpty())
+                completionScores.add(getCompletionScore(stack));
         }
 
-        System.out.println(totalErrorScore);
+        Collections.sort(completionScores);
+        System.out.println(completionScores);
+        System.out.println(completionScores.size());
+        long winner = completionScores.get(((completionScores.size() + 1) / 2) - 1);
+        System.out.println(winner);
 
+    }
+
+    public static long getCompletionScore(Stack<Character> stack) {
+        long score = 0;
+        String openChars = "([{<";
+        while(!stack.isEmpty()) {
+            char c = stack.pop();
+            if(openChars.indexOf(c) != -1) {
+                switch (c) {
+                    case '(' -> {
+                        score = score * 5;
+                        score += 1;
+                    }
+                    case '[' -> {
+                        score = score * 5;
+                        score += 2;
+                    }
+                    case '{' -> {
+                        score = score * 5;
+                        score += 3;
+                    }
+                    case '<' -> {
+                        score = score * 5;
+                        score += 4;
+                    }
+                    default -> System.out.println("Error!");
+                }
+            } else
+                stack.pop();
+        }
+
+        return score;
     }
 }
