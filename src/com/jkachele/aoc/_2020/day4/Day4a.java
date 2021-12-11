386ddc3602a2dea2172e99a520fea6d58d9f91ca
Justin Kachele <justin@kachele.com>
2021-12-11 14:22:57 -0600

    initial commit

--- src/com/jkachele/aoc/_2020/day4/Day4a.java
+++ src/com/jkachele/aoc/_2020/day4/Day4a.java
@@ -3,6 +3,7 @@ package com.jkachele.aoc._2020.day4;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Day4a {
@@ -16,5 +17,37 @@ public class Day4a {
         }
         fileIn.close();
         System.out.println(lines);
+
+        ArrayList<ArrayList<String>> passports = new ArrayList<>();
+        ArrayList<String> passport = new ArrayList<>();
+
+        for(String line: lines) {
+            if(!line.isEmpty()) {
+                String[] lineSplit = line.split(" ");
+                passport.addAll(Arrays.asList(lineSplit));
+            } else {
+                passports.add(passport);
+                passport = new ArrayList<>();
+            }
+        }
+        passports.add(passport);
+
+
+        int goodPassports = 0;
+
+        for(ArrayList<String> p: passports) {
+            int fields = 0;
+            String cidRegex = "cid:\\d+";
+            for(String field : p) {
+                if(!field.matches(cidRegex)) {
+                    fields++;
+                }
+            }
+            if(fields == 7)
+                goodPassports++;
+        }
+
+        System.out.println(goodPassports);
+
     }
 }
