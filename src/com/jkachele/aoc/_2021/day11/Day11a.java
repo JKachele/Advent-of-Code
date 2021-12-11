a91e594e068fdc30babfd931cba94d28b4b0fd5f
Justin Kachele <justin@kachele.com>
2021-12-11 02:47:36 -0600

    initial commit

---
+++ src/com/jkachele/aoc/_2021/day11/Day11a.java
@@ -0,0 +1,20 @@
+package com.jkachele.aoc._2021.day11;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day11a {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("E:\\JavaProjects\\Advent-of-Code\\src\\com\\jkachele\\aoc\\_2021\\day11\\input.txt");
+        Scanner fileIn = new Scanner(file);
+        ArrayList<String> lines = new ArrayList<>();
+
+        while (fileIn.hasNext()) {
+            lines.add(fileIn.nextLine());
+        }
+        fileIn.close();
+        System.out.println(lines);
+    }
+}
