e910de63b4b251b80beb2042be7e9de8c684a9f5
Justin Kachele <justin@kachele.com>
2021-12-11 22:32:57 -0600

    initial commit

---
+++ src/com/jkachele/aoc/_2020/day5/Day5b.java
@@ -0,0 +1,20 @@
+package com.jkachele.aoc._2020.day5;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day5b {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src/com/jkachele/aoc/_2020/day5/input.txt");
+        Scanner fileIn = new Scanner(file);
+        ArrayList<String> lines = new ArrayList<>();
+
+        while (fileIn.hasNext()) {
+            lines.add(fileIn.nextLine());
+        }
+        fileIn.close();
+        System.out.println(lines);
+    }
+}
