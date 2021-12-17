a8d2142280dceaefedafc8861faa385521e994da
Justin Kachele <Justin@kachele.com>
2021-12-16 22:18:14 -0600

    initial commit

---
+++ src/com/jkachele/aoc/_2021/day13/Day13a.java
@@ -0,0 +1,19 @@
+package com.jkachele.aoc._2021.day13;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day13a {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src/com/jkachele/aoc/_2021/day13/input.txt");
+        Scanner fileIn = new Scanner(file);
+        ArrayList<String> lines = new ArrayList<>();
+
+        while (fileIn.hasNext())
+            lines.add(fileIn.nextLine());
+        fileIn.close();
+        System.out.println(lines);
+    }
+}
