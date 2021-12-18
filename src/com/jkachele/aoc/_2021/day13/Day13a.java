b5e83f0ad759f34b3864bd1e888b25651be26a74
Justin Kachele <Justin@kachele.com>
2021-12-18 11:52:03 -0600

    initial commit

--- src/com/jkachele/aoc/_2021/day13/Day13a.java
+++ src/com/jkachele/aoc/_2021/day13/Day13a.java
@@ -15,5 +15,30 @@ public class Day13a {
             lines.add(fileIn.nextLine());
         fileIn.close();
         System.out.println(lines);
+
+        ArrayList<Integer[]> dots = new ArrayList<>();
+
+        //loop to get the coordinates of the dots into an integer array and to determine the largest position of a dot
+        int maxNum = 0;
+        int lineNum = 0;
+        while(!lines.get(lineNum).isEmpty()) {
+            String line = lines.get(lineNum);
+            String[] dotString = line.split(",");
+            int x = Integer.parseInt(dotString[0]);
+            int y = Integer.parseInt(dotString[1]);
+            if(x > maxNum)
+                maxNum = x;
+            else if(y > maxNum)
+                maxNum = y;
+            Integer[] dot = {x, y};
+            dots.add(dot);
+            lineNum++;
+        }
+
+        boolean[][] paper = new boolean[maxNum][maxNum];
+
+        for(Integer[] dot : dots) {
+            paper[dot[0]][dot[1]] = true;
+        }
     }
 }
