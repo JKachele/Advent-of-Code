07c8d96ffc8a3e645792ee7e74221b0f22c09e09
Justin Kachele <Justin@kachele.com>
2022-08-14 13:37:27 -0600

    Initial Commit

--- src/com/jkachele/aoc/_2021/day15/Day15b.java
+++ src/com/jkachele/aoc/_2021/day15/Day15b.java
@@ -5,16 +5,29 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Day15b {
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("src/com/jkachele/aoc/_2021/day15/input.txt");
-        Scanner fileIn = new Scanner(file);
-        ArrayList<String> lines = new ArrayList<>();
+public class day15b {
+    public static void main(String[] args) {
+        String fileName = "src/com/jkachele/aoc/_2021/day15/input.txt";
+        //String fileName = "src/com/jkachele/aoc/_2021/day15/testInput.txt";
+        ArrayList<String> lines = readFile(fileName);
 
-        while (fileIn.hasNext()) {
-            lines.add(fileIn.nextLine());
+    }
+
+    public static ArrayList<String> readFile(String fileName) {
+        ArrayList<String> lines = new ArrayList<>();
+        try {
+            File file = new File(fileName);
+            Scanner fileIn = new Scanner(file);
+
+            while (fileIn.hasNext()) {
+                lines.add(fileIn.nextLine());
+            }
+            fileIn.close();
+            System.out.println(lines);
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found");
+            System.exit(0);
         }
-        fileIn.close();
-        System.out.println(lines);
+        return lines;
     }
 }
