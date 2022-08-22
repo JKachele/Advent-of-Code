46b7368be33e7dcba1d6cf1c8a01db976bd2707c
Justin Kachele <Justin@kachele.com>
2022-08-22 11:55:36 -0400

    Initial Commit

--- src/com/jkachele/aoc/_2019/day1/Day1b.java
+++ src/com/jkachele/aoc/_2019/day1/Day1b.java
@@ -3,6 +3,7 @@ package com.jkachele.aoc._2019.day1;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Day1b {
@@ -11,6 +12,24 @@ public class Day1b {
         //String fileName = "src/com/jkachele/aoc/_2019/day1/testInput.txt";
         ArrayList<String> lines = readFile(fileName);
 
+        int[] fuelList = new int[lines.size()];
+        for (int i = 0; i < lines.size(); i++) {
+            int mass = Integer.parseInt(lines.get(i).trim());
+            int fuel = (mass / 3) - 2;
+
+            while(fuel > 0) {
+                fuelList[i] += fuel;
+                fuel = (fuel / 3) - 2;
+            }
+        }
+        System.out.println(Arrays.toString(fuelList));
+
+        int totalFuel = 0;
+        for(int fuel: fuelList) {
+            totalFuel += fuel;
+        }
+
+        System.out.println(totalFuel);
     }
 
     public static ArrayList<String> readFile(String fileName) {
